cc9b1389702d031330080a858642c2d29e124835
tristanable <tristanleecj@gmail.com>
2023-11-04 04:51:51 +0800

    intermeditate commit, did not commit properly earlier and lost function that kept limit to 2 numbers on display

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ let previousResult = 0;
 
 let currentNumber = {
     num: '',
-    status: false, //true means it is activated;
 }
 
-let op = '+';
+let op = '';
 
 let nextNumber = {
     num: '',
-    status: true,
 }
 
-
+const buttonContainer = document.querySelector('.buttonContainer');
 const displayOutput = document.querySelector('.displayContainer');
 const equalButton = document.querySelector('.equalButton');
 const zeroButton = document.querySelector('.zeroButton');
@@ -21,6 +19,7 @@ const nodeListOfNumberButtons = document.querySelectorAll('.numberButton'); //th
 const numberButtons = Array.from(nodeListOfNumberButtons); //makes nodelist into an array
 const nodelistOfOperatorButtons = document.querySelectorAll('.operatorButton');
 const operatorButtons = Array.from(nodelistOfOperatorButtons);
+const powerBtn = document.querySelector('.powerBtn');
 const clearBtn = document.querySelector('.clearBtn');
 
 function operate(a, op, b) {
@@ -38,7 +37,7 @@ function operate(a, op, b) {
             return a * b;
             break;
         case '/':
-            return a / b;
+            return parseFloat((a/b).toFixed(5));
             break;
         case '^':
             return a ** b;
@@ -64,18 +63,20 @@ zeroButton.addEventListener('click', () => {
             displayOutput.innerHTML += '0';
             break;
     };
-    onEquals();
 });
 
 numberButtons.map(button => {
     button.addEventListener('click', () => { 
+        clearScreenAfterError()
         let text = button.textContent;
         //console.log(`text is: ${text}`);
         switch (ticker%2) {
             case 0: //not yet pressed/ holding a value, so allocate 0 to 'a'
-                currentNumber['num'] += text;
+                currentNumber['num'] += text; //eg pressing 3, text = '3'
                 let a = currentNumber['num'];
-                displayOutput.innerHTML += text;
+                let numberOfCharactersOnDisplay = displayOutput.textContent;
+                sliceValueToFitDisplay(numberOfCharactersOnDisplay);
+                displayOutput.innerText += text;
                 console.log(`we are on a: ${a}`);
                 //console.log(typeof +a);
                 break;
@@ -99,6 +100,11 @@ operatorButtons.map(button => {
     });
 });
 
+powerBtn.addEventListener('click', () => {
+    op = '^'
+    ticker += 1;
+    displayOutput.innerText += op;
+})
 
 clearBtn.addEventListener('click', () => {
     displayOutput.innerText = '';
@@ -107,25 +113,56 @@ clearBtn.addEventListener('click', () => {
     ticker = 0;
 })
 
+equalButton.addEventListener('click', onEquals);
 
 function onEquals() {
-    equalButton.addEventListener('click', () => {
-        let numbersOnDisplay = displayOutput.textContent;
-        console.log(typeof numbersOnDisplay);
-        console.log(`numbers On Display is: ${numbersOnDisplay}`);
-        numbersOnDisplay = numbersOnDisplay.trim();
-        numbersOnDisplay = numbersOnDisplay.split(op);
-        let a = +numbersOnDisplay[0];
-        let b = +numbersOnDisplay[1];
-        console.log(typeof numbersOnDisplay);
-        console.log(numbersOnDisplay);
-        console.log(`a after split is: ${a}`);
-        console.log(`b after split is: ${b}`);
-        console.log(`op is: ${op}`);
-        let result = operate(a, op, b);
-        displayOutput.innerText = result;
-        previousResult = result;
-});
+    let numbersOnDisplay = displayOutput.textContent;
+    //console.log(typeof numbersOnDisplay);
+    //console.log(`numbers On Display is: ${numbersOnDisplay}`);
+    numbersOnDisplay = numbersOnDisplay.trim();
+    numbersOnDisplay = numbersOnDisplay.split(op);
+    let a = +numbersOnDisplay[0];
+    let b = +numbersOnDisplay[1];
+    //console.log(typeof numbersOnDisplay);
+    console.log(numbersOnDisplay);
+    console.log(`a after split is: ${a}`);
+    console.log(`b after split is: ${b}`);
+    console.log(`op is: ${op}`);
+    let result = operate(a, op, b);
+    result = sliceValueToFitDisplay(result);
+    displayOutput.innerText = result;
+    previousResult = result;
+
+    op = '';
+    currentNumber['num'] = '';
+    nextNumber['num'] = '';
 }
 
-onEquals()
+function clearScreenAfterError() {
+    if (displayOutput.textContent == NaN || displayOutput.textContent == 'Syntax Error' || displayOutput.textContent == Infinity) {
+        displayOutput.innerText = '';
+    } else {
+        return;
+    }
+}
+
+function sliceValueToFitDisplay(value) {
+    value = value.toString();
+    if (value.length > 11) {
+        value = value.slice(0,12);
+        displayOutput.innerText = value;
+        return value;
+    } else {
+        return value;
+    }
+} 
+
+function computeCurrentTwoValues() {
+    let screenItems = displayOutput.textContent;
+    screenItems = screenItems.split(''); //make it into an array of [a, op, b]
+    if (screenItems.length >= 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
