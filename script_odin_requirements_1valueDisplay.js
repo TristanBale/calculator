37d3a86b31dba3052160c3f4944d10dc1f78944d
tristanable <tristanleecj@gmail.com>
2023-11-06 06:52:56 +0800

    fixed issue with DEL key on very first computation, fixed issues with decimal leading to error and not allowing currentOperand to have decimal

--- script_odin_requirements_1valueDisplay.js
+++ script_odin_requirements_1valueDisplay.js
@@ -13,10 +13,11 @@ const id = [previousOperand, currentOperand];
 
 let op = '';
 let isOperatorPressed = false;
-let previousResult = '';
+let previousResult = 'default'; //starting of we dont use '' as with how its prgrammed, we always check if previousResult is '' for when we press clear and the operand status keys are handled accordingly to a clear or delete till empty. But the very first input needs to be handled diferently
 
 const isOperatorPressedDisplay = document.querySelector('.isOperatorPressed');
 const displayOutput = document.querySelector('.displayContainer');
+displayOutput.innerText = '';
 
 const nodeListOfNumberButtons = document.querySelectorAll('.numberButton'); //this is a nodelist
 const numberButtons = Array.from(nodeListOfNumberButtons); //makes nodelist into an array
@@ -25,7 +26,7 @@ const decimalButton = document.querySelector('.decimalButton');
 
 const nodelistOfOperatorButtons = document.querySelectorAll('.operatorButton');
 const operatorButtons = Array.from(nodelistOfOperatorButtons);
-const powerBtn = document.querySelector('.powerBtn');
+//const powerBtn = document.querySelector('.operatorButton');
 
 const clearBtn = document.querySelector('.clearBtn');
 const deleteBtn = document.querySelector('.deleteBtn');
@@ -72,11 +73,18 @@ numberButtons.map(button => {
 
 zeroButton.addEventListener('click', () => {
         let number = zeroButton.textContent;
+            
+        if (isNaN(previousResult)) {
+            displayOutput.innerText = '';
+            previousResult = '';
+            currentOperand['num'] = '';
+        }
+
         if (displayOutput.textContent == previousResult) { // this if statement ensures that if a number is pressed after a final result, the screen changes to that number instead of appending to the back of finalResult 
             displayOutput.innerText = '';
-            previousOperand['num'] = previousResult;
-            previousOperand['status'] = false,
-            currentOperand['status'] = true;
+            previousOperand['num'] = '';
+            previousOperand['status'] = true,
+            currentOperand['status'] = false;
         } 
 
         if (!isOperatorPressed) {
@@ -100,18 +108,15 @@ zeroButton.addEventListener('click', () => {
         checkStatus(id)
 })
 
+
 decimalButton.addEventListener('click', () => {
     let decimal = decimalButton.textContent;
 
-    if (decimalLimit(id)) {
-       return;
-    }
-
     if (displayOutput.textContent == previousResult) {
         displayOutput.innerText = '';
-        previousOperand['num'] = previousResult;
-        previousOperand['status'] = false;
-        currentOperand['status'] = true;
+        previousOperand['num'] = '';
+        previousOperand['status'] = true;
+        currentOperand['status'] = false;
     }
 
     if (!isOperatorPressed) {
@@ -145,13 +150,18 @@ operatorButtons.map(button => {
     button.addEventListener('click', () => {
         previousOperand['status'] = false;
         let operator = button.textContent;
+        console.log('');
         console.log(`op is ${operator}`);
         checkStatus(id);
-        if (previousOperand['num'] == ''){
-            return;
+
+        if ((displayOutput.textContent == '' || displayOutput.textContent == previousResult) && operator == '-') {
+            previousOperand['status'] = true;
+            displayOutput.innerText += operator;
+            isOperatorPressed = false; //only false for this case as we want the number to be input to remain as previousOperand, but how our code works is that it will automatically change to current after an operator is pressed, but as you can see, this wont work if the screen was not empty
+            isOperatorPressedDisplay.innerText = isOperatorPressed;
         }
 
-        if (op == '') {
+        else if (op == '') {
             op = operator;
             displayOutput.innerText += op;
             isOperatorPressed = true;
@@ -173,6 +183,7 @@ clearBtn.addEventListener('click', () => {
 deleteBtn.addEventListener('click', () => {
     let currentItemsOnDisplay = displayOutput.innerText;
     let poppedItem = currentItemsOnDisplay.slice(-1);
+    console.log('');
     console.log(`poppedItem is ${poppedItem}`);
     currentItemsOnDisplay = currentItemsOnDisplay.slice(0,-1);
     displayOutput.innerText = currentItemsOnDisplay;
@@ -229,8 +240,9 @@ function operate(a, op, b) {
 function onEquals() {
     let result = operate(previousOperand['num'], op, currentOperand['num']);
     console.log('');
-    console.log(`previousOperand is ${previousOperand['num']}`);
-    console.log(`currentOperand is ${currentOperand['num']}`);
+    console.log(`previousOperand: ${previousOperand['num']}`);
+    console.log(op);
+    console.log(`currentOperand: ${currentOperand['num']}`);
 
     previousResult = result;
     if (previousOperand['num'] === '') {
@@ -268,7 +280,8 @@ function onEquals() {
 function checkStatus(array) {
     array.filter(operand => {
         if (operand.status == true) {
-            console.log(`operand that is true is ${operand.name}`);
+            console.log('');
+            console.log(`operand:  ${operand.name}`);
             console.log(operand);
             return operand;
         }
@@ -279,8 +292,15 @@ function decimalLimit(array) {
     let previousOperand = array.find(operand => operand.name === 'prev');
     let currentOperand = array.find(operand => operand.name === 'current');
 
-    let previousHasDecimal = previousOperand['num'].includes('.');
-    let currentHasDecimal = currentOperand['num'].includes('.');
+    let previousHasDecimal = String(previousOperand['num']).includes('.');
+    let currentHasDecimal = String(currentOperand['num']).includes('.');
 
-    return previousHasDecimal && currentHasDecimal;
+    return previousHasDecimal || currentHasDecimal;
 }
+
+
+
+
+//current issues identified with code, cant do .5 + .2 //solved
+//initial 6 + 5 del 7 gives 7 instead of 13. //solved
+//
